cd7197ec4c893c25b182413837865edd43f0ceb7
Izac Moreira Souza Junior <izac.junior1@estudante.ufla.br>
2026-04-19 11:31:30 -0300

    Criacao das classes A e B com atributos e metodos solicitados

---
+++ A.ts
@@ -0,0 +1,25 @@
+class A {
+    private A1: number;
+    private A2: number;
+
+    constructor(a1: number, a2: number) {
+        this.A1 = a1;
+        this.A2 = a2;
+    }
+
+    // Getters e Setters
+    public getA1(): number { return this.A1; }
+    public setA1(valor: number): void { this.A1 = valor; }
+
+    public getA2(): number { return this.A2; }
+    public setA2(valor: number): void { this.A2 = valor; }
+
+    // Métodos solicitados
+    public MA1(): void {
+        console.log("MA1");
+    }
+
+    public MA2(): void {
+        console.log("MA2");
+    }
+}
---
+++ B.ts
@@ -0,0 +1,25 @@
+class B {
+    private B1: number;
+    private B2: number;
+
+    constructor(b1: number, b2: number) {
+        this.B1 = b1;
+        this.B2 = b2;
+    }
+
+    // Getters e Setters
+    public getB1(): number { return this.B1; }
+    public setB1(valor: number): void { this.B1 = valor; }
+
+    public getB2(): number { return this.B2; }
+    public setB2(valor: number): void { this.B2 = valor; }
+
+    // Métodos solicitados
+    public MB1(): void {
+        console.log("MB1");
+    }
+
+    public MB2(): void {
+        console.log("MB2");
+    }
+}
